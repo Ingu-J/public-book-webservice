fcd29e80de4534b3b84a798eacd4be17d4c16450
ingu <cik2925@gmail.com>
2022-06-27 23:23:41 +0900

    chapter 10.4까지 부분 완료 수정 2

--- script/deploy.sh
+++ script/deploy.sh
@@ -5,10 +5,11 @@
 
 REPOSITORY=/home/ec2-user/app/step3
 PROJECT_NAME=public-book-webservice
+SCRIPT_DIR=script
 
-echo "> Start $REPOSITORY/$PROJECT_NAME/stop.sh"
-sh $REPOSITORY/$PROJECT_NAME/stop.sh
-echo "> Start $REPOSITORY/$PROJECT_NAME/start.sh"
-sh $REPOSITORY/$PROJECT_NAME/start.sh
-echo "> Start $REPOSITORY/$PROJECT_NAME/health.sh"
-sh $REPOSITORY/$PROJECT_NAME/health.sh
+echo "> Start $REPOSITORY/$PROJECT_NAME/$SCRIPT_DIR/stop.sh"
+sh $REPOSITORY/$PROJECT_NAME/$SCRIPT_DIR/stop.sh
+echo "> Start $REPOSITORY/$PROJECT_NAME/$SCRIPT_DIR/start.sh"
+sh $REPOSITORY/$PROJECT_NAME/$SCRIPT_DIR/start.sh
+echo "> Start $REPOSITORY/$PROJECT_NAME/$SCRIPT_DIR/health.sh"
+sh $REPOSITORY/$PROJECT_NAME/$SCRIPT_DIR/health.sh
